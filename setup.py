dd877fe0b404459486e0ca893ef37476c54b4d8b
gyounghun6612 <gyounghun6612@gmail.com>
2021-02-23 13:57:12 +0900

    update some git+https install

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name="AIS_",
+    name="AIS_utils",
     version="0.0.1",
     description="Custom code module in python",
-    url="git@github.com:gyounghun6612/base_utils_v2.git",
+    url="https://github.com/gyounghun6612/base_utils_v2.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
     packages=["AIS_utils"],
