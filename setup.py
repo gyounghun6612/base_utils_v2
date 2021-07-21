81d68d0f2d171cc3db7165ac4e47c49d2307272c
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2021-07-21 16:58:59 +0900

    update for git install

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="AIS_utils",
-    version="0.0.1",
+    name="AIS_python_utils",
+    version="2.0.0",
     description="Custom base code module for python",
-    url="https://github.com/gyounghun6612/base_utils_v2.git",
+    url="https://github.com/SEOULTECH-AIS-gyounghun6612/python_base_utils.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
     packages=["ais_utils"],
     zip_safe=False,
     install_requires=[
-        "opencv-python"
+        "opencv-python", "numpy"
     ]
 )
